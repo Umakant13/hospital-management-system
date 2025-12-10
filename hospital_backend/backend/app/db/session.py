d3a9b24e8c9f8e89b259663ea5f40a4fc5b473d1
Umakant Dodtalle <113049057+Umakant13@users.noreply.github.com>
2025-12-10 16:33:33 +0530

    Update session.py

--- hospital_backend/backend/app/db/session.py
+++ hospital_backend/backend/app/db/session.py
@@ -6,8 +6,9 @@ engine = create_engine(
     settings.DATABASE_URL,
     echo=settings.DATABASE_ECHO,
     pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20
+    pool_size=2,          # render supports max 5
+    max_overflow=1,       # keep very low
+    pool_recycle=1800     # avoid stale connections
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
